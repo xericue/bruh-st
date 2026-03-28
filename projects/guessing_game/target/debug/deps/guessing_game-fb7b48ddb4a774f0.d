81fd0c7a8df37f26f767889f81b5d1e68e1e05d9
Kris <xericue@yahoo.com>
2026-03-28 09:34:26 -0400

    guessing game!

---
+++ projects/guessing_game/target/debug/deps/guessing_game-fb7b48ddb4a774f0.d
@@ -0,0 +1,5 @@
+/mnt/c/projects/bruh-st/projects/guessing_game/target/debug/deps/guessing_game-fb7b48ddb4a774f0.d: src/main.rs
+
+/mnt/c/projects/bruh-st/projects/guessing_game/target/debug/deps/guessing_game-fb7b48ddb4a774f0: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/mnt/c/projects/bruh-st/projects/guessing_game/target/debug/guessing_game: /mnt/c/projects/bruh-st/projects/guessing_game/src/main.rs
